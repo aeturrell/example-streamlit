fe1947842e6606827c7a67f6cc77702a7aff8139
aeturrell <a.e.turrell@gmail.com>
2022-03-13 16:47:31 +0000

    a very basic version done

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ import streamlit as st
 import altair as alt
 import pandas as pd
 import numpy as np
+import datetime
 import matplotlib.pyplot as plt
 import pandasdmx as pdmx
+import pandas_datareader as pdr
 
 
 # Plot settings
@@ -11,17 +13,100 @@ plt.style.use(
     "https://github.com/aeturrell/coding-for-economists/raw/main/plot_style.txt"
 )
 
+
+# Functions
+@st.cache
+def get_oecd_data():
+    # Tell pdmx we want OECD data
+    oecd = pdmx.Request("OECD")
+    # Set out everything about the request in the format specified by the OECD API
+    data = oecd.data(
+        resource_id="QNA",
+        key="GBR." + "+".join(codes) + ".LNBQRSA.Q/all?startTime=2015",
+    ).to_pandas()
+
+    df_oecd = pd.DataFrame(data).reset_index()
+    # some clean up operations
+    df_oecd["datetime"] = (
+        pd.to_datetime(
+            df_oecd["TIME_PERIOD"].apply(
+                lambda x: str(x[:4]) + "-" + str(int(x[-1]) * 3)
+            ),
+            format="%Y-%m",
+        )
+        + pd.offsets.MonthEnd()
+    )
+    df_oecd["value"] = df_oecd["value"]
+    df_oecd["industry"] = df_oecd["SUBJECT"].map(industry_dict)
+    df_oecd = df_oecd.sort_values(["industry", "datetime"])
+    # find the greatest growing sector within each time-sector cell
+    # first compute growth within industry
+    df_oecd["growth"] = df_oecd.groupby(["industry"])["value"].transform(
+        lambda x: x.pct_change()
+    )
+    # now max growth within each time-period
+    df_oecd["max"] = df_oecd.groupby("datetime")["growth"].transform(lambda x: x.max())
+    df_oecd["argmax"] = (
+        df_oecd.groupby(["datetime"])["growth"]
+        .transform(lambda x: x.idxmax())
+        .astype("Int32")
+    )
+    df_oecd["max_ind"] = pd.NA
+    df_oecd.loc[:, "max_ind"] = df_oecd.loc[
+        df_oecd["argmax"].fillna(0), "industry"
+    ].values
+    # and min
+    df_oecd["min"] = df_oecd.groupby("datetime")["growth"].transform(lambda x: x.min())
+    df_oecd["argmin"] = (
+        df_oecd.groupby(["datetime"])["growth"]
+        .transform(lambda x: x.idxmin())
+        .astype("Int32")
+    )
+    df_oecd["min_ind"] = pd.NA
+    df_oecd.loc[:, "min_ind"] = df_oecd.loc[
+        df_oecd["argmin"].fillna(0), "industry"
+    ].values
+    # compute a total
+    df_oecd["GDP"] = df_oecd.groupby(["datetime", "LOCATION"])["value"].transform(
+        lambda x: x.sum()
+    )
+    # then shares as a pct
+    df_oecd["fraction"] = 100 * df_oecd["value"] / df_oecd["GDP"]
+    # then find the industry with greatest fraction in each time period
+    df_oecd["max_frac"] = df_oecd.groupby("datetime")["fraction"].transform(
+        lambda x: x.max()
+    )
+    df_oecd["argmax_frac"] = (
+        df_oecd.groupby(["datetime"])["fraction"]
+        .transform(lambda x: x.idxmax())
+        .astype("Int32")
+    )
+    df_oecd["frac_ind"] = pd.NA
+    df_oecd.loc[:, "frac_ind"] = df_oecd.loc[
+        df_oecd["argmax_frac"].fillna(0), "industry"
+    ].values
+    return df_oecd
+
+
+# Rest of dashboard
+
 st.title("An example dashboard")
 # Here's some text...
-intro_text = "This is a short example dashboard demonstrating the key capabilities of streamlit."
+intro_text = (
+    "This is a short example dashboard demonstrating the key capabilities of streamlit."
+)
 # ...but it only appears in the script when we call `st.write`
 st.write(intro_text)
 
 # Here's some markdown
 st.markdown("### This is a markdown subtitle")
-st.markdown("Regular markdown synthax, such as [links](https://aeturrell.github.io/coding-for-economists), works. You can use `st.latex` to embed latex equations. Here's the result of using that command:")
+st.markdown(
+    "Regular markdown syntax, inlcuding [links](https://aeturrell.github.io/coding-for-economists), will work. You can use `st.latex` to embed latex equations. Here's the result of using that command:"
+)
 # Here's an example of a latex equation:
-st.latex(r"{\displaystyle {\frac {\partial L}{\partial q^{i}}}(t,{\boldsymbol {q}}(t),{\dot {\boldsymbol {q}}}(t))-{\frac {\mathrm {d} }{\mathrm {d} t}}{\frac {\partial L}{\partial {\dot {q}}^{i}}}(t,{\boldsymbol {q}}(t),{\dot {\boldsymbol {q}}}(t))=0,\quad i=1,\dots ,n.}")
+st.latex(
+    r"{\displaystyle {\frac {\partial L}{\partial q^{i}}}(t,{\boldsymbol {q}}(t),{\dot {\boldsymbol {q}}}(t))-{\frac {\mathrm {d} }{\mathrm {d} t}}{\frac {\partial L}{\partial {\dot {q}}^{i}}}(t,{\boldsymbol {q}}(t),{\dot {\boldsymbol {q}}}(t))=0,\quad i=1,\dots ,n.}"
+)
 
 
 st.markdown("### Data")
@@ -43,69 +128,79 @@ industry_dict = {
 codes = list(industry_dict.keys())
 industry_names = list(industry_dict.values())
 
-
-@st.cache
-def get_oecd_data():
-    # Tell pdmx we want OECD data
-    oecd = pdmx.Request("OECD")
-    # Set out everything about the request in the format specified by the OECD API
-    data = oecd.data(
-        resource_id="QNA",
-        key="GBR."
-        + "+".join(codes)
-        + ".LNBQRSA.Q/all?startTime=2015",
-    ).to_pandas()
-
-    df_oecd = pd.DataFrame(data).reset_index()
-    # some clean up operations
-    df_oecd["datetime"] = (
-        pd.to_datetime(
-            df_oecd["TIME_PERIOD"].apply(lambda x: str(x[:4]) + "-" + str(int(x[-1]) * 3)),
-            format="%Y-%m",
-        )
-        + pd.offsets.MonthEnd()
-    )
-    df_oecd["value"] = df_oecd["value"]
-    df_oecd["industry"] = df_oecd["SUBJECT"].map(industry_dict)
-    # compute a total
-    df_oecd["GDP"] = df_oecd.groupby(["datetime", "LOCATION"])["value"].transform(
-        lambda x: x.sum()
-    )
-    # then shares as a pct
-    df_oecd["fraction"] = df_oecd["value"] / df_oecd["GDP"]
-    return df_oecd
-
 df = get_oecd_data()
 
 st.write("Data downloaded successfully! First few lines:")
 st.write(df.head())
 
 st.markdown("### Metrics")
-st.write("Next, we'll use the columns functionality to show which sectors are the biggest movers on the quarter")
+st.write(
+    "Next, we'll use the columns functionality to show which sectors are the biggest movers on the quarter making use of OECD data and industry categories."
+)
 
 # want to compute these using the last timestep available in data
-# TODO
+# subset to most recent data
+subdf = df.loc[df["datetime"] == df["datetime"].max(), :]
+rise_ind, rise_growth = subdf["max_ind"].iloc[0], subdf["max"].iloc[0] * 100
+rise_share = subdf.loc[subdf["industry"] == rise_ind, "fraction"]
+fall_ind, fall_growth = subdf["min_ind"].iloc[0], subdf["min"].iloc[0] * 100
+fall_share = subdf.loc[subdf["industry"] == fall_ind, "fraction"]
 
-col1, col2, col3 = st.columns(3)
-col1.metric(f"Biggest rise:", "70 °F", "1.2 °F")
-col2.metric(f"Biggest fall:", "9 mph", "-8%")
-col3.metric(f"Biggest share:", "86%", "4pp")
+st.write(f"#### For quarter ending {df['datetime'].max():%Y-%m-%d}")
+
+col1, col2 = st.columns(2)
+col1.metric("Biggest rise:", f"{rise_ind}", f"{rise_growth:.2f} %")
+col2.metric("Biggest fall:", f"{fall_ind}", f"{fall_growth:.2f} %")
 
 st.markdown("### Charts")
 st.markdown("#### Altair")
-st.write("We're going to try plotting data on UK output by sector sourced from the OECD.")
+st.write(
+    "We're going to try plotting data on UK output by sector sourced from the OECD."
+)
 # Let's give users a multi-select box to choose industries
-chosen_industries = st.multiselect("Choose which industries to display",
-                                    industry_names,
-                                    default=industry_names)
+chosen_industries = st.multiselect(
+    "Choose which industries to display", industry_names, default=industry_names
+)
 # Filter OECD data just to chosen industries
-subdf = df[df['industry'].isin(chosen_industries)]
+subdf = df[df["industry"].isin(chosen_industries)]
 
-graph = alt.Chart(subdf).mark_bar(size=15).encode(
-    alt.Y('value:Q', scale=alt.Scale(domain=(0, 600e3))),
-        x='datetime:T',
-        color='industry',
-        tooltip=['industry', subdf.columns[-1]]
-    ).properties(title="UK output by sector (chained volume, seasonally adjusted; mn GBP)").interactive()
+graph = (
+    alt.Chart(subdf)
+    .mark_bar(size=15)
+    .encode(
+        alt.Y("value:Q", scale=alt.Scale(domain=(0, 600e3))),
+        x="datetime:T",
+        color="industry",
+        tooltip=["industry", subdf.columns[-1]],
+    )
+    .properties(
+        title="UK output by sector (chained volume, seasonally adjusted; mn GBP)"
+    )
+    .interactive()
+)
 st.altair_chart(graph, use_container_width=True)
 st.write("Try changing the multi-select box above to see how the chart changes.")
+
+st.markdown("#### Matplotlib")
+
+logscale = st.checkbox("Log scale?", False)
+
+import pandas_datareader.data as web
+
+start = datetime.datetime(1919, 1, 1)
+end = datetime.datetime(2016, 1, 1)
+fred_uk_awe = web.DataReader("AWEPPUKQ", "fred", start, end)
+
+
+scale_txt = "linear scale"
+if logscale:
+    scale_txt = "log scale"
+
+fig, ax = plt.subplots()
+ax.plot(fred_uk_awe)
+ax.set_ylim(1, None)
+ax.set_ylabel("Seasonally adjusted GBP")
+ax.set_title(f"Average weekly earnings per person in the UK ({scale_txt})", loc="left")
+if logscale:
+    ax.set_yscale("log")
+st.pyplot(fig)
